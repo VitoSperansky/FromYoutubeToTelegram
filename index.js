83ad60efee5d3c2926b6ee9767f91fdf02d75292
Vito Speransky <vitosperansky@gmail.com>
2024-08-27 14:07:22 +0500

    bugfix again

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import https from 'node:https';
 import { OAuth2Client } from 'google-auth-library'
-import logger from 'pino';
+
+import pino from 'pino'
+const logger = pino();
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
