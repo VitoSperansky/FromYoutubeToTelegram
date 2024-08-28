f9e40e891f19096b3039b5304fe624d267b18c9b
Vito Speransky <vitosperansky@gmail.com>
2024-08-28 18:37:14 +0500

    yandex metric for page

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ bot.start(async (ctx) => {
 
 
     await setBotCommands()
-    ctx.replyWithHTML('<b>Приветствуем вас в нашем сервисе поиска Telegram-каналов ютуберов!</b>\nБот безопасен, так как представляет собой открытый исходный код, который может посмотреть каждый желающий. (/faq или пишите @vitosperansky)\n\nПоддержать проект - https://www.donationalerts.com/r/vitosperansky\n\nВыберите опцию:', Markup.inlineKeyboard([
+    ctx.replyWithHTML('<b>Приветствуем вас в нашем сервисе поиска Telegram-каналов ютуберов!</b>\nБот безопасен, так как представляет собой открытый исходный код, который может посмотреть каждый желающий. (/faq или пишите @vitosperansky)\n\nПоддержать проект: https://www.donationalerts.com/r/vitosperansky\n\nВыберите опцию:', Markup.inlineKeyboard([
         [Markup.button.callback('Найти YouTube-каналы в Telegram', 'find_channels')],
         [Markup.button.callback('Связать YouTube-канал с Telegram-каналом', 'link_channel')]
     ]), {
