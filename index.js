7d391c9d2f205271f14e08f23df4d66217031baf
Vito Speransky <vitosperansky@gmail.com>
2024-08-29 16:02:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const analyticsSchema = new mongoose.Schema({
     chatId: { type: String, unique: true },
     awatingChannels: Boolean,
     status: String,
-    count: Number,
+    count: Int32,
 },
     {
         timestamps: true
